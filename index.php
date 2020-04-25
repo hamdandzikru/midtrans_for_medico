a5d37a1408fb7a24c3ecd3eb375b34595c29cf88
hamdan_dz <hamdanrobbi@gmail.com>
2020-04-25 20:01:38 +0700

    resolve

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ $is_production = false;
 $api_url = $is_production ? 'https://app.midtrans.com/snap/v1/transactions': 'https://app.sandbox.midtrans.com/snap/v1/transactions';
 
 if(!strpos($_SERVER['REQUEST_URI'], '/charge')){
-
+	http_response_code(404)
+	echo "wrong path"; exit();
 }
 
 if($_SERVER['REQUEST_METHOD'] !== 'POST'){
